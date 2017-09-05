1d87071c6be45e25a9d344c4deb7ef86a489a8e6
Angelo Baruffi <angelobaruffi2@gmail.com>
2017-09-05 20:10:48 -0300

    Update in text cleaning

--- dataMining.py
+++ dataMining.py
@@ -4,37 +4,48 @@ Created on Sat Sep 02 11:11:19 2017
 
 @author: Angelo Baruffi e Andrei Donati
 """
-import numpy as np
-import pandas as pd
-
-from read import load
-from sklearn.feature_extraction.text import TfidfVectorizer
-from nltk.stem.snowball import SnowballStemmer
-from nltk.corpus import stopwords
-
 ## soup = BeautifulSoup(open("test.html").read())
 ## h1 = soup.find_all('h1') retorna uma lista de elementos com a tag h1 no formato: <h1>Jeff Baggett</h1>.
 ## >>> <h1>Jeff Baggett</h1>.text
 ## Jeff Baggett
 
 
-soups = data['student'][:5]
+start_time = time.time()
+
+try:
+    time
+except NameError:
+    import time
+    
+    import numpy as np
+    import pandas as pd
+    import re
+    
+    from read import load
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from nltk.stem.snowball import SnowballStemmer
+    #from nltk.corpus import stopwords
+
+try:
+    data
+except NameError:
+    data = load()
+
+
+soups = data['student'][:2]
 stemmer = SnowballStemmer("english") # Choose a language
 
 text = np.array([soup.get_text() for soup in soups])
 
-df_part = df[:500] 
 
-text = df_part['title'][:]
 
-corpus = process_text(text)
-
-def process_text(texts):
+def process_text(texts): #Limpeza dos textos - Essa função não está mais sendo usada.
     texts = texts.copy()
     sw = stopwords.words('english')    
     m = len(texts)
     corpus = np.array([])
-    for index in xrange(m):
+    
+    for index in xrange(m): #Faz o processamento para cada texto da lista
         texts[index] = texts[index].replace('\n',' ')
         texts[index] = texts[index].replace('\t',' ')
         texts[index] = texts[index].replace('.',' ')
@@ -53,9 +64,9 @@ def process_text(texts):
 
        
         texts[index] = stemmer.stem(texts[index])
-        for word in sw:
-            texts[index] = texts[index].replace(''.join((' ',word,' ')), " ")
-        
+#        for word in sw:
+#            texts[index] = texts[index].replace(''.join((' ',word,' ')), " ")
+#        
         splited = texts[index].split()
         text = ''
         for word in np.array(splited).copy():
@@ -69,3 +80,30 @@ def process_text(texts):
         corpus = np.append(corpus,text)
         
     return corpus
+
+def clean_texts(texts):
+    # column you are working on
+    minF = 0.05
+    maxF = 0.95   
+   
+    df_ = pd.Series(texts)
+    
+    #stopword_set = set(stopwords.words("english"))
+    
+    # convert to lower case and split 
+    df_ = df_.str.lower().str.split()
+    
+#    # remove stopwords
+#    df_ = df_.apply(lambda x: [item for item in x if item not in stopword_set])
+    
+    # keep only words
+    df_ = df_.apply(lambda x: [re.sub(r'[^a-zA-Z\s]', '', element, flags=re.IGNORECASE) for element in x])
+    
+    df_ = df_.str.join(' ')
+    # join the cleaned words in a list
+    return df_
+
+corpus = clean_texts(text)
+    
+
+print("--- %s seconds ---" % (time.time() - start_time))
